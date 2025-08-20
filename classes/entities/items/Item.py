a16202531142f9ed000475b2658f1105e4463efd
Tyler Pearson <tyler.a.pear@gmail.com>
2025-08-20 10:09:34 -0500

    refactors Item.py to use property getters and setters

--- classes/entities/items/Item.py
+++ classes/entities/items/Item.py
@@ -2,8 +2,28 @@
 
 class Item:
   def __init__(self):
-    self.equipped = False
-    self.active = False
+    self._equipped = False
+    self._active = False
+
+### PROPERTIES ###
+
+  @property
+  def equipped(self):
+    return self._equipped
+
+  @equipped.setter
+  def equipped(self, equipped):
+    self._equipped = equipped
+
+  @property
+  def active(self):
+    return self._active
+
+  @active.setter
+  def active(self, active):
+    self._active = active
+
+### METHODS ###
 
   def update(self, player):
     self.direction = player.direction
