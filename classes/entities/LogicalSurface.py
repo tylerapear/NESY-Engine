c9777c23a724dd384367aa9507c4c50d1b8463a7
Tyler Pearson <tyler.a.pear@gmail.com>
2025-08-25 21:18:49 -0500

    refactors LogicalSurface.py to use property getters

--- classes/entities/LogicalSurface.py
+++ classes/entities/LogicalSurface.py
@@ -2,14 +2,34 @@ import pygame
 
 class LogicalSurface():
   def __init__(self, width, height, background_color):
-    self.surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+    self._surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
     self._width = width
     self._height = height
     self._background_color = background_color
     
+  # PROPERTIES #
+  
+  @property
+  def surface(self):
+    return self._surface
+  
+  @property
+  def width(self):
+    return self._width
+  
+  @property
+  def height(self):
+    return self._height
+  
+  @property
+  def background_color(self):
+    return self._background_color
+  
+  # METHODS #
+    
   def compute_fit(self, dst_w, dst_h): 
-    scale = min(dst_w / self._width, dst_h / self._height) 
-    render_w, render_h = int(self._width * scale), int(self._height * scale) 
+    scale = min(dst_w / self.width, dst_h / self.height) 
+    render_w, render_h = int(self.width * scale), int(self.height * scale) 
     x_off = (dst_w - render_w) // 2 
     y_off = (dst_h - render_h) // 2 
     return scale, x_off, y_off, render_w, render_h 
@@ -18,9 +38,9 @@ class LogicalSurface():
     dst_w, dst_h = surface.get_size() 
     scale, x_off, y_off, render_w, render_h = self.compute_fit(dst_w, dst_h) 
     
-    surface.fill(self._background_color) 
+    surface.fill(self.background_color) 
     
-    if render_w == self._width and render_h == self._height: 
+    if render_w == self.width and render_h == self.height: 
       surface.blit(self.surface, (x_off, y_off)) 
     else: 
       scaled = pygame.transform.smoothscale(self.surface, (render_w, render_h)) 
